817f7077e4fb8ea9e0186f3b38cf270c3c788e53
W1891513 <167537355+W1891513@users.noreply.github.com>
2024-04-25 00:49:32 +0100

    apps.py

--- item/apps.py
+++ item/apps.py
@@ -1,5 +1,10 @@
+# Import AppConfig from django.apps, which is the base class for all app configurations in Django.
 from django.apps import AppConfig
 
-
+# Define a new configuration class named InventoryManagerConfig.
+# This class inherits from AppConfig.
 class InventoryManagerConfig(AppConfig):
+    # Set the name of the application. This is a mandatory attribute.
+    # 'name' is used by Django to identify the application.
+    # It should match the Python path to the application, typically the name of the directory that holds the app.
     name = 'item'
