059df1f9198a1a4ed8e32b7e1348d5fd0a62ed54
W1891513 <167537355+W1891513@users.noreply.github.com>
2024-04-25 00:47:34 +0100

    admin.py

--- item/admin.py
+++ item/admin.py
@@ -1,4 +1,8 @@
+# Import the admin module from Django's built-in 'contrib' package.
 from django.contrib import admin
+# Import the Item model from the local 'models' module within the same application.
 from .models import Item
 # Register your models here.
+# This line makes the Item model accessible via the Django admin interface.
+# By registering the model, you enable CRUD operations on Item instances from the admin panel.
 admin.site.register(Item)
