2e626d13d601a5715680f54228f7503b73b48d4d
Muneeb <167630085+Muneebandco@users.noreply.github.com>
2024-04-24 22:58:25 +0100

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,22 +1,24 @@
-# Django imports
-from django.urls import path
-# Project imports
-from . import views
+"""inventory URL Configuration
 
-# App namespace
-app_name = "accounts"
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
-# URL patterns for account-related views
 urlpatterns = [
-    # URL pattern for user signup
-    path('signup/', views.UserCreateView.as_view(), name='signup'),
-    
-    # URL pattern for user login
-    path('login/', views.CustomLoginView.as_view(), name='login'),
-    
-    # URL pattern for user logout
-    path('logout/', views.LogoutView.as_view(), name="logout"),
-    
-    # URL pattern for user profile view
-    path('profile/<pk>', views.UserDetailView.as_view(), name='profile'),
+    path('admin/', admin.site.urls),
+    path('', include('item.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('store/', include('store.urls'))
 ]
