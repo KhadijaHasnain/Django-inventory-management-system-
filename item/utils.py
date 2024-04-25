e933dcb743ef861c1e1647087577e52ad09f1d97
W1891513 <167537355+W1891513@users.noreply.github.com>
2024-04-25 01:06:36 +0100

    utils.py

--- item/utils.py
+++ item/utils.py
@@ -1,25 +1,28 @@
+# Import get_random_string from django.utils.crypto to generate a random sequence of characters.
 from django.utils.crypto import get_random_string
 import re
 from django.db.models import Q
 
-# Function to generate a random string
+# Function to generate a random string, which serves as a part of the unique identifier.
 def random_string_generator():
+        # The function returns a string of 10 characters long, chosen from the specified set of uppercase letters and numbers.
     return get_random_string(length=10, allowed_chars="ABCDEF0123456789")
 
-# Function to generate a unique item ID
+# The function returns a string of 10 characters long, chosen from the specified set of uppercase letters and numbers.
 def unique_item_id_generator(instance):
-    # Generate a random string
+    # Generate a random string to be potentially used as a unique item ID.
     item_new_id = random_string_generator()
 
-    # Get the class of the instance
+    # Retrieve the class of the instance. This allows the function to be model-agnostic, working with any Django model instance.
     Item = instance.__class__
 
-    # Check if any other item with the same ID exists
+    # Check if there is an existing record in the database with the same item ID.
     qs_exists = Item.objects.filter(item_id=item_new_id).exists()
 
-    # If another item with the same ID exists, generate a new ID
+    # If a record with the same item ID already exists, recursively call the function to generate a new ID.
+    # This recursion will continue until a unique ID is generated.
     if qs_exists:
         return unique_item_id_generator(instance)
     
-    # Return the unique item ID
+    # Return the unique item ID after confirming it's unique.
     return item_new_id
